027320c6f2bdf3a13bb9d56ad77091773b562834
luutienphat <luutienphat@gmail.com>
2020-09-05 21:52:48 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+(function() {
+    const link = document.querySelectorAll('.hover-this');
+    const cursor = document.querySelector('.cursor');
+
+    const animateit = function(e) {
+        const span = this.querySelector('span');
+
+        const {offsetX: x, offsetY: y} = e;
+        const {offsetWidth: width, offsetHeight: height} = this;
+
+        const move = 25;
+
+        xMove = x / width * (move * 2) - move,
+        yMove = y / height * (move * 2) - move;
+
+        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+        if(e.type === 'mouseleave') span.style.transform = '';
+    }
+
+    const editCursor = e => {
+        console.log(e);
+        const { clientX: x, clientY: y } = e;
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
+})();
